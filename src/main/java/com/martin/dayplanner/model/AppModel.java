f0a8cd087c0b2ad2ec3ecc441c8c5718f4e4296f
@Martin.Romskaug <marom4137@uib.no>
2025-01-06 13:33:25 +0100

    ok løsning, men navigasjon, og ulike plasnners går

--- src/main/java/com/martin/dayplanner/model/AppModel.java
+++ src/main/java/com/martin/dayplanner/model/AppModel.java
@@ -6,27 +6,13 @@ import com.martin.dayplanner.view.ViewableAppModel;
 public class AppModel implements ViewableAppModel, ControllableAppModel {
 
     private final HomeScreen homeScreenModel;
-    private final Planner plannerModel;
     private Object activeModel;
+    private String activePlanner;
 
     public AppModel() {
         this.homeScreenModel = new HomeScreen(this);
-        this.plannerModel = new Planner("My first plan", this);
-        this.activeModel = plannerModel;
-    }
-
-    @Override
-    public HomeScreen getHomeScreenModel() {
-        return homeScreenModel;
-    }
-
-    @Override
-    public Planner getPlannerModel() {
-        return plannerModel;
-    }
-
-    public void goToMenu() {
-        activeModel = homeScreenModel;
+        this.activeModel = homeScreenModel;
+        this.activePlanner = null;
     }
 
     @Override
@@ -39,7 +25,24 @@ public class AppModel implements ViewableAppModel, ControllableAppModel {
         Planner selectedPlanner = homeScreenModel.findPlannerByName(plannerName);
         if (selectedPlanner != null) {
             activeModel = selectedPlanner;
+            activePlanner = plannerName;
             System.out.println("Active model changed to Planner: " + plannerName);
         }
+    }
+
+    public void goToMenu() {
+        this.activeModel = homeScreenModel;
+        this.activePlanner = null;
+    }
+
+    @Override
+    public HomeScreen getHomeScreenModel() {
+        return homeScreenModel;
+    }
+
+    @Override
+    public Planner getPlannerModel() {
+        Planner plannerModel = homeScreenModel.findPlannerByName(activePlanner);
+        return plannerModel;
     }
 }
