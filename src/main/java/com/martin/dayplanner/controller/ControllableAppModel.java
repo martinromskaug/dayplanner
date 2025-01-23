57d4526d1f3cfa07eb9127f91da665ab93ba28e0
@Martin.Romskaug <marom4137@uib.no>
2025-01-23 13:05:20 +0100

    javadoc

--- src/main/java/com/martin/dayplanner/controller/ControllableAppModel.java
+++ src/main/java/com/martin/dayplanner/controller/ControllableAppModel.java
@@ -5,10 +5,25 @@ import com.martin.dayplanner.controller.planner.ControllablePlanner;
 
 public interface ControllableAppModel {
 
+    /**
+     * Returns the home screen model.
+     * 
+     * @return the home screen model
+     */
     public ControllableHomeScreen getHomeScreenModel();
 
+    /**
+     * Returns the planner model.
+     * 
+     * @return the planner model
+     */
     public ControllablePlanner getPlannerModel();
 
+    /**
+     * Returns the active model.
+     * 
+     * @return the active model
+     */
     public Object getActiveModel();
 
 }
